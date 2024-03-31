a16a4b532d5d76e70ed1821a365266a2ab43b165
kimyezzang97 <kimyezzang97@gmail.com>
2024-03-31 20:37:41 +0900

    회원가입 라우팅 처리 (route.js)

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,13 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
-
+  {
+    path: '/member',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: 'join', component: () => import('pages/member/JoinPage.vue') },
+    ],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
