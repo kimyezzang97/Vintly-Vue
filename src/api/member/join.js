3ef083e1fb4f981bdcb7de8f683e44b4552881b3
kimyezzang97 <kimyezzang97@gmail.com>
2024-04-05 19:25:22 +0900

    member join - 중복확인 API들 추가

---
+++ src/api/member/join.js
@@ -0,0 +1,19 @@
+import { api } from 'src/boot/axios';
+
+// 중복 ID 조회
+export const getChkId = async id => {
+  const data = await api.get(`/member/id/{id}`);
+  return data;
+};
+
+// 중복 email 조회
+export const getChkEmail = async email => {
+  const data = await api.get(`/member/email/{email}`);
+  return data;
+};
+
+// 중복 nickname 조회
+export const getChkNickname = async nickname => {
+  const data = await api.get(`/member/nickname/{nickname}`);
+  return data;
+};
