dd9b5ac520b199b127b0a44dc92b1ecf03c81424
kimyezzang97 <kimyezzang97@gmail.com>
2024-04-28 23:20:32 +0900

    join Page url 경로 변경

--- src/api/member/join.js
+++ src/api/member/join.js
@@ -2,23 +2,23 @@ import { api } from 'src/boot/axios';
 
 // 중복 ID 조회
 export const getChkId = async id => {
-  const data = await api.get(`/member/id/${id}`);
+  const data = await api.get(`/members/id/${id}`);
   return data;
 };
 
 // 중복 email 조회
 export const getChkEmail = async email => {
-  const data = await api.get(`/member/email/${email}`);
+  const data = await api.get(`/members/email/${email}`);
   return data;
 };
 
 // 중복 nickname 조회
 export const getChkNickname = async nickname => {
-  const data = await api.get(`/member/nickname/${nickname}`);
+  const data = await api.get(`/members/nickname/${nickname}`);
   return data;
 };
 
 export const postMemberJoin = async joinForm => {
-  const data = await api.post(`/member/join`, joinForm);
+  const data = await api.post(`/members/join`, joinForm);
   return data;
 };
